8f8f6cd5a296a7e39d70eb44195c883731eb3de9
Joe Vogel <joejvogel@gmail.com>
2023-10-03 16:21:06 -0300

    ADD Project and README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # mestrado-imu-reader
+
+This repository contains an implementation of a program to read the [VectorNav VN-300 IMU sensor](https://www.vectornav.com/products/detail/vn-300) data and convert it to the [SmartData MotionVector](https://lisha.ufsc.br/SDAV+-+SmartData+Model) for Autonomous Vehicles development
+
+## Program file
+
+This is a C++ project, copied from VectorNav library and inside the [/examples/imu_reader](./examples/imu_reader) is the main sourcecode [main.cpp](./examples/imu_reader/main.cpp)
+
+You can use Make to compile the file to generate the [imu_reader](./examples/imu_reader/imu_reader) executable to your Operational System.
+
+Execute this command inside [imu_reader](./examples/imu_reader/) to compile the runnable file:
+
+	make .
+
+And then, execute the [imu_reader](./examples/imu_reader/imu_reader) file with the command below (on Linux):
+
+	./imu_reader
---
+++ examples/imu_reader/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+
+// Include this header file to get access to VectorNav sensors.
+#include "vn/sensors.h"
+
+// We need this file for our sleep function.
+#include "vn/thread.h"
+
+using namespace std;
+using namespace vn::math;
+using namespace vn::sensors;
+using namespace vn::protocol::uart;
+using namespace vn::xplat;
+
+// Method declarations for future use.
+void asciiAsyncMessageReceived(void* userData, Packet& p, size_t index);
+void asciiOrBinaryAsyncMessageReceived(void* userData, Packet& p, size_t index);
+
+int main(int argc, char *argv[])
+{
+	const string SensorPort = "/dev/ttyUSB0";                  // Linux format for virtual (USB) serial port.
+	const uint32_t SensorBaudrate = 115200;
+
+	// Now let's create a VnSensor object and use it to connect to our sensor.
+	VnSensor vs;
+	vs.connect(SensorPort, SensorBaudrate);
+
+	vs.writeAsyncDataOutputType(VNISL);
+
+  	AsciiAsync asyncType = vs.readAsyncDataOutputType();
+	cout << "ASCII Async Type: " << asyncType << endl;
+
+	vs.registerAsyncPacketReceivedHandler(NULL, asciiAsyncMessageReceived);
+
+	// Now sleep for 5 seconds so that our asynchronous callback method can
+	// receive and display receive yaw, pitch, roll packets.
+	// TODO: remove when production use
+	cout << "Starting sleep..." << endl;
+	Thread::sleepSec(5);
+
+	// Unregister our callback method.
+	// TODO: when running in production, do this only when process stops
+	vs.unregisterAsyncPacketReceivedHandler();
+
+	vs.disconnect();
+
+	return 0;
+}
+
+void asciiAsyncMessageReceived(void* userData, Packet& p, size_t index)
+{
+  // Make sure we have an ASCII packet and not a binary packet.
+  if(p.type() != Packet::TYPE_ASCII)
+    return;
+
+  // Make sure we have a VNISL data packet.
+  if (p.determineAsciiAsyncType() == VNISL) {
+
+	vec3f ypr;
+	vec3d lla;
+	vec3f velocity;
+	vec3f acceleration;
+	vec3f angularRate;
+	p.parseVNISL(&ypr, &lla, &velocity, &acceleration, &angularRate);
+
+	cout << "YPR: " << ypr << endl;
+	cout << "lla: " << lla << endl;
+	cout << "velocity: " << velocity << endl;
+	cout << "acceleration: " << acceleration << endl;
+	cout << "angularRate: " << angularRate << endl;
+  }
+
+  else {
+    cout << "ASCII Async: Type(" << p.determineAsciiAsyncType() << ")" << endl;
+  }
+}
